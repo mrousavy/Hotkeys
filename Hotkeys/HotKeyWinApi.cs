b27f5703f284198712a6f91cf68462574272d9d4
Marc Rousavy <Marc Rousavy>
2017-01-10 18:04:59 +0100

    Initial Upload

---
+++ Hotkeys/HotKeyWinApi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+
+namespace CPBuffer
+{
+    public class HotKeyWinApi
+    {
+        public const int WmHotKey = 0x0312;
+
+        [DllImport("user32.dll",SetLastError = true)]
+        public static extern bool RegisterHotKey(IntPtr hWnd,int id,ModifierKeys fsModifiers,int vk);
+
+        [DllImport("user32.dll",SetLastError = true)]
+        public static extern bool UnregisterHotKey(IntPtr hWnd,int id);
+    }
+}
